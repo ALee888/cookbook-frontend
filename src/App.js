c369782dc6fef8e397ffb0ae4797615751ca3d4e
Adam Lee <alee11@zagmail.gonzaga.edu>
2023-09-06 13:41:00 -0700

    Changing to include routes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,56 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
+import { BrowserRouter as Router, Route, Link, Routes } from 'react-router-dom';
 import './App.css';
+import RecipeInfo from './RecipeInfo';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [recipes, setRecipes] = useState([]);
+
+    // GET with fetch API
+    useEffect(() => {
+        const fetchPost = async () => {
+            const response = await fetch(
+                'http://localhost:4000/recipes'
+            );
+            const data = await response.json();
+            console.log(data);
+            setRecipes(data.result);
+        };
+        fetchPost();
+    }, []);
+    
+    return (
+        
+        <Router>
+            <div className='app'>
+                <header>
+                    <h1 id='title'>Our Cookbook</h1>
+                </header>
+                <div className='recipes'>
+                    <table>
+                        <tr>
+                            <th>id</th>
+                            <th>name</th>
+                            <th>date</th>
+                        </tr>
+                        {recipes.map((recipe, index) => (
+                            <tr key={index} className='recipe'>    
+                                <td className='id'>{recipe.id}</td>
+                                <td>
+                                    <Link to={`/recipe/${recipe.id}`}>{recipe.name}</Link>
+                                </td>
+                                <td>{recipe.created_at}</td>
+                            </tr>
+                        ))}
+                    </table>
+                </div>
+                <Routes>
+                    <Route path='/' element={<div>Recipes</div>} />
+                    <Route path="/recipe/:recipeId" element={<RecipeInfo />} />
+                </Routes>
+            </div>
+        </Router>
+    )
 }
 
 export default App;
---
+++ src/Recipes.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+function Recipes() {
+
+    return (
+        <table>
+            <tr>
+                <th>Name</th>
+                <th>Ingredients</th>
+                <th>Instructions</th>
+            </tr>
+            for each recipe in sql:
+                <tr>
+                    with info
+                </tr>
+        </table>
+    );
+}
+
+export default Recipes;
