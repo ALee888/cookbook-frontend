b088189d7185a6a7290a27d5ff26aa310145e808
Adam Lee <alee11@zagmail.gonzaga.edu>
2023-09-11 15:32:34 -0700

    Create Recipe Functionality

--- src/pages/NewRecipe.js
+++ src/pages/NewRecipe.js
@@ -4,33 +4,56 @@ function NewRecipe () {
     const [recipe, setRecipe] = useState({
         name: '',
         description: '',
-        ingredients: [],
-    })
+        ingredients: [{ name: '', quanitty: '', measurement: ''}],
+    });
 
     // Handle input changes for recipe details
     const handleInputChange = (e) => {
         const { name, value } = e.target;
         setRecipe({ ...recipe, [name]: value})
-    }
+    };
 
     // Handle adding a new ingredient
     const addIngredient = () => {
-        const newIngredients = [...recipe.ingredients, ''];
+        const newIngredients = [...recipe.ingredients, { name: '', quanitty: '', measurement: ''}];
         setRecipe({ ...recipe, ingredients: newIngredients});
     };
 
     // Handle changing an ingredient's value
     const handleIngredientChange = (e, index) => {
+        const { name, value } = e.target;
         const newIngredients = [...recipe.ingredients];
-        newIngredients[index] = e.target.value;
+        newIngredients[index][name] = value;
+
         setRecipe({ ...recipe, ingredients: newIngredients});
     };
 
     // Handle form submission
-    const handleSubmit = (event) => {
-        event.preventDefault();
-        alert(`${recipe.name}\n${recipe.description}`)
-    }
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        
+        // Call API
+		fetchPost();
+        
+        // Reset Form
+        setRecipe({
+            name: '',
+            description: '',
+            ingredients: []
+        })
+    };
+
+    // Fetch POST Request
+    const fetchPost = async () => {
+        const response = await fetch(`http://localhost:4000/recipes`, {
+            method: 'POST',
+            headers: {
+                "Content-Type": 'application/json',
+            },
+            body: JSON.stringify(recipe),
+        });
+        console.log(response.json());
+    };
 
     return (
         <div className='newRecipe'>
@@ -45,34 +68,48 @@ function NewRecipe () {
                     required
                 />
                 {/*Name, description and ingredients [{name, quantity, measurements}]*/}
-                <label htmlFor='description'>
-                    <textarea name="message" rows="10" cols="30" onChange={handleInputChange}/>
-                </label>
-                <div className='ingredientInput'>
-                    <label> name
-                        <input 
+                <label htmlFor='description'>Description:</label>
+                <textarea 
+                    id='description'
+                    name="description" 
+                    value = {recipe.description} 
+                    onChange={handleInputChange}
+                    required
+                />
+                <label>Ingredients:</label>
+                {recipe.ingredients.map((ingredient, index) => (
+                    <div key={index}>
+                        <input
                             type='text'
-                            onChange={handleIngredientChange}
+                            name='name'
+                            value={ingredient.name}
+                            onChange={(e) => handleIngredientChange(e, index)}
+                            placeholder='Ingredient Name'
+                            required
                         />
-                    </label>
-                    <label> quantity
-                        <input 
-                            type='number'
-                            onChange={handleIngredientChange}
-                        />
-                    </label>
-                    <label> measurement
-                        <input 
+                        <input
                             type='text'
-                            onChange={handleIngredientChange}
+                            name='quantity'
+                            value={ingredient.quantity}
+                            onChange={(e) => handleIngredientChange(e, index)}
+                            placeholder='Quantity'
                         />
-                    </label>
-                    <button type="button" onclick={addIngredient}>Add Ingredient +</button>
-                </div>
+                        <input
+                            type='text'
+                            name='measurement'
+                            value={ingredient.measurement}
+                            onChange={(e) => handleIngredientChange(e, index)}
+                            placeholder='Measurement'
+                        />
+                    </div>
+                ))}
+                <button type='button' onClick={addIngredient}>
+                    Add Ingredient
+                </button>
                 <input type="submit" value={'submit'}/>
             </form>
         </div>
-    )
+    );
 }
 
 export default NewRecipe;
