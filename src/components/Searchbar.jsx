1cf74d7d2b641faec9ef50950afa96414b65c621
Adam Lee <alee11@zagmail.gonzaga.edu>
2023-10-23 12:35:05 -0700

    Working Search Bar

--- src/components/Searchbar.jsx
+++ src/components/Searchbar.jsx
@@ -1,17 +1,53 @@
 import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
 
-function SearchBar() {
-  const [text, setText] = useState('');
+const SearchBar = (props) => {
+    const [search, setSearch] = useState('');
+    let recipes = props.data;
 
-  const handleInputChange = (event) => {
-    setText(event.target.value);
-  };
+    const handleInputChange = (event) => {
+        event.preventDefault();
+        setSearch(event.target.value);
+    };
 
-  return (
-    <div className='SearchBar'>
-      <input type="text" value={text} onChange={handleInputChange} />
-    </div>
-  );
+    if (search.length > 0) {
+        recipes.filter((recipe) => {
+            console.log(recipe.name, search)
+            return recipe.name.match(search);
+        });
+        console.log(recipes)
+    }
+
+    return (
+        <div className='SearchBar'>
+            <input type="text" value={search} onChange={handleInputChange} />
+            <table>
+                <tr>
+                    <th>id</th>
+                    <th>name</th>
+                    <th>date</th>
+                </tr>
+                {recipes.map((recipe, index) => (
+                    <tr key={index} className='recipe'>    
+                        <td>{recipe.id}</td>
+                        <td>
+                            <Link 
+                                to={`/recipes/${recipe.id}`}
+                                state={{
+                                    name: recipe.name,
+                                    description: recipe.description,
+                                    instructions: recipe.instructions,
+                                    created_at: recipe.created_at
+                                }}>    
+                                {recipe.name}
+                            </Link>
+                        </td>
+                        <td>{recipe.created_at}</td>
+                    </tr>
+                ))}
+            </table>
+        </div>
+    );
 }
 
 export default SearchBar;
--- src/pages/Recipes.js
+++ src/pages/Recipes.js
@@ -4,7 +4,21 @@ import SearchBar from '../components/Searchbar';
 
 function Recipes() {
     const [recipes, setRecipes] = useState([]);
+    const [searchInput, setSearchInput] = useState('');
+    const [filteredRecipes, setFilteredRecipes] = useState(recipes);
 
+    const handleInputChange = (event) => {
+        event.preventDefault();
+        setSearchInput(event.target.value);
+        if (searchInput.length > 0) {
+            const filteredRecipes = recipes.filter((recipe) => {
+                return recipe.name.match(searchInput);
+            });
+            setFilteredRecipes(filteredRecipes);
+        }
+    };
+
+    
     // GET with fetch API
     useEffect(() => {
         const fetchPost = async () => {
@@ -20,32 +34,61 @@ function Recipes() {
     return (
         <div className='recipes'>
             <h1>Recipes</h1>
-            <SearchBar/>
-            <table>
-                <tr>
-                    <th>id</th>
-                    <th>name</th>
-                    <th>date</th>
-                </tr>
-                {recipes.map((recipe, index) => (
-                    <tr key={index} className='recipe'>    
-                        <td>{recipe.id}</td>
-                        <td>
-                            <Link 
-                                to={`/recipes/${recipe.id}`}
-                                state={{
-                                    name: recipe.name,
-                                    description: recipe.description,
-                                    instructions: recipe.instructions,
-                                    created_at: recipe.created_at
-                                }}>    
-                                {recipe.name}
-                            </Link>
-                        </td>
-                        <td>{recipe.created_at}</td>
+            <input type="text" value={searchInput} onChange={handleInputChange} />
+            {searchInput === '' ? (
+                <table>
+                    <tr>
+                        <th>id</th>
+                        <th>name</th>
+                        <th>date</th>
                     </tr>
-                ))}
-            </table>
+                    {recipes.map((recipe, index) => (
+                        <tr key={index} className='recipe'>    
+                            <td>{recipe.id}</td>
+                            <td>
+                                <Link 
+                                    to={`/recipes/${recipe.id}`}
+                                    state={{
+                                        name: recipe.name,
+                                        description: recipe.description,
+                                        instructions: recipe.instructions,
+                                        created_at: recipe.created_at
+                                    }}>    
+                                    {recipe.name}
+                                </Link>
+                            </td>
+                            <td>{recipe.created_at}</td>
+                        </tr>
+                    ))}
+                </table>
+            ) : (
+                <table>
+                    <tr>
+                        <th>id</th>
+                        <th>name</th>
+                        <th>date</th>
+                    </tr>
+                    {filteredRecipes.map((recipe, index) => (
+                        <tr key={index} className='recipe'>    
+                            <td>{recipe.id}</td>
+                            <td>
+                                <Link 
+                                    to={`/recipes/${recipe.id}`}
+                                    state={{
+                                        name: recipe.name,
+                                        description: recipe.description,
+                                        instructions: recipe.instructions,
+                                        created_at: recipe.created_at
+                                    }}>    
+                                    {recipe.name}
+                                </Link>
+                            </td>
+                            <td>{recipe.created_at}</td>
+                        </tr>
+                    ))}
+                </table>
+            )}
+            
         </div>
     );
 }
