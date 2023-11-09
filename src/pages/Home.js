dc05ddbd69d7613b41e5f3ec3834d4bcc7340617
Adam Lee <alee11@zagmail.gonzaga.edu>
2023-11-09 11:46:55 -0800

    added searchBar to home page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import searchImage from './search.png'
 
 const Home = () => {
+    const [searchInput, setSearchInput] = useState('');
+    const handleInputChange = (event) => {
+        event.preventDefault();
+        setSearchInput(event.target.value);
+    };
     return (
         <div className="home">
             <h1>Our Cookbook!</h1>
+            <div className="searchbar">
+                <input type="text" value={searchInput} onChange={handleInputChange} />
+                <Link to={`/recipes`}><img src={searchImage} alt="search"/></Link>
+            </div>
         </div>
     );
 };
